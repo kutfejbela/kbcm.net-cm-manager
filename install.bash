612fac619290d6801b2df3aa2d87f4ea3d6d2512
kutfejbela <kutfejbela@gmail.com>
2017-06-22 13:03:38 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # /etc
-/bin/mkdir -p "/etc/kbnet.com-cm/manager-server"
+/bin/mkdir -p "/etc/kbcom.net-cm/manager-server"
 
 # /usr/lib/cgi-bin
-/bin/mkdir -p "/usr/lib/cgi-bin/kbcom.net-cm"
-/bin/ln -s "$PWD/cgi-bin" "/usr/lib/cgi-bin/kbcom.net-cm/manager-server"
+if [ ! -e "/usr/lib/cgi-bin/kbcom.net-cm/manager-server" ]
+then
+ /bin/mkdir -p "/usr/lib/cgi-bin/kbcom.net-cm"
+ /bin/ln -s "$PWD/cgi-bin" "/usr/lib/cgi-bin/kbcom.net-cm/manager-server"
+else
+ /bin/echo "'/usr/lib/cgi-bin/kbcom.net-cm/manager-server' does exists"
+fi
 
 # /usr/share
-/bin/mkdir -p "/usr/share/kbcom.net-cm"
-/bin/ln -s "$PWD/share" "/usr/share/kbcom.net-cm/manager-server"
+if [ ! -e "/usr/share/kbcom.net-cm/manager-server" ]
+then
+ /bin/mkdir -p "/usr/share/kbcom.net-cm"
+ /bin/ln -s "$PWD/share" "/usr/share/kbcom.net-cm/manager-server"
+else
+ /bin/echo "'/usr/share/kbcom.net-cm/manager-server' does exists"
+fi
